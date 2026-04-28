9a0e2ef9d46af6c4ab4ed9cdf2e5849d1d7f17d4
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2026-04-28 19:39:10 -0300

    feat: Adicionada rota /admin em src/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Admin } from './pages/admin/admin';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home,
-    }
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: '/admin',
+    component: Admin,
+  },
 ];
