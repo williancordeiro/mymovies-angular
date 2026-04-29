5e665ce57277601419a6297fd7a510807d907015
FelipeFadel <dasilvafadelfelipe@gmail.com>
2026-04-29 00:02:53 -0300

    Mudança de cores no login-form

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,12 +1,16 @@
 import { Component, output, signal } from '@angular/core';
-
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './login-form.html',
 })
 export class LoginForm {
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+
   closeForm = output<void>();
 
   goToRegister = output<void>();
