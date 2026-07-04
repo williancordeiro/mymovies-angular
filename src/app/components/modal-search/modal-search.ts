549871819e5c82d46986da2bc12280e71fe43f49
williancordeiro <williancordeiro369@gmail.com>
2026-07-04 16:24:49 -0300

    Feature: busca agora retorna resultado apartir de 3 caracteres

--- src/app/components/modal-search/modal-search.ts
+++ src/app/components/modal-search/modal-search.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, output } from '@angular/core';
+import { SearchService } from '../../core/services/search.service';
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-modal-search',
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './modal-search.html',
 })
-export class ModalSearch {}
+export class ModalSearch {
+  public searchService = inject(SearchService);
+  closeModal = output<void>();
+
+  @HostListener('document:keydown.escape')
+  onKeydownHandler() {
+    this.closeModal.emit();
+  }
+}
