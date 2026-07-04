8571737ff1703341819e4d487d8c36cceb2c1534
williancordeiro <williancordeiro369@gmail.com>
2026-07-04 16:25:57 -0300

    Feature: adicionado card-movie exclusivo para os resultados de busca

---
+++ src/app/components/card-movie-search-result/card-movie-search-result.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { Movie } from '../../core/models/movie';
+
+@Component({
+  selector: 'app-card-movie-search-result',
+  imports: [RouterLink, FontAwesomeModule, DatePipe, DecimalPipe],
+  templateUrl: './card-movie-search-result.html',
+})
+export class CardMovieSearchResult {
+  @Input({ required: true }) movie!: Movie;
+  faStar = faStar;
+}
