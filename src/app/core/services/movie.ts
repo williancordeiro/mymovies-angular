86eeca87b0376b415de3445f3537e6425d0b102f
williancordeiro <williancordeiro369@gmail.com>
2026-07-05 23:14:25 -0300

    Doc: Removido codigo comentado

--- src/app/core/services/movie.ts
+++ src/app/core/services/movie.ts
@@ -19,10 +19,6 @@ export class MovieService {
     return this.http.get<any>(`${this.API_URL}/movies/${id}`);
   }
 
-  /*saveRating(movieId: number, rating: number): Observable<any> {
-    return this.http.post(`${this.API_URL}/movies/rate`, { movie_id: movieId, rating: rating });
-  }*/
-
   saveRating(movieId: number, rating: number, tagsId: number[] = []): Observable<any> {
     const body = {
       movie_id: movieId,
